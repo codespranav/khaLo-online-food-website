4b815874036bcd765af6c7dabe765fe332143bb1
Pranav <pranavk24h@gmail.com>
2024-09-19 22:59:05 +0530

    add shadcn ui theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Mail } from "lucide-react";
 export default function Home() {
   return (
-    <h1>Welcome to Pizza Shop</h1>
+    <>
+      <h1>Welcome to Pizza Shop</h1>
+      <Button>
+      <Mail className="mr-2 h-4 w-4" /> Login with Email
+    </Button>
+    </>
+
   );
 }
