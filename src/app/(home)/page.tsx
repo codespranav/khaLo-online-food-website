236713bc97b89681ea1b3f2e48cd07a69b291a01
Pranav <pranavk24h@gmail.com>
2024-09-20 22:54:22 +0530

    add hero section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,23 @@
 import { Button } from "@/components/ui/button";
-import { Mail } from "lucide-react";
+import { ClockArrowUp, Mail } from "lucide-react";
+import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="font-manrope">Welcome to Pizza Shop</h1>
-      <Button>
-      <Mail className="mr-2 h-4 w-4" /> Login with Email
-    </Button>
+      <section className="bg-white mt-8">
+          <div className="max-w-full md:max-w-6xl md:m-auto flex md:justify-between items-center flex-wrap">
+              <div>
+                  <h1 className="text-6xl font-black font-sans leading-tight">Super Delicious Food in <br />
+                  <span className="text-primary ">Only 45 minutes!</span>
+                  </h1>
+                  <p className="text-2xl mt-8 md:max-w-lg leading-snug">Enjoy free meal if your order takes more than 45 minutes 🚚</p>
+                  <Button className="mt-8 text-lg rounded-full py-7 px-6 font-bold">Order Now <ClockArrowUp className="ml-4" /></Button>
+              </div>
+              <div>
+                <Image src= "/food.jpeg" alt="Food" width={400} height={400} className="rounded-md"/>
+              </div>
+          </div>
+      </section>
     </>
 
   );
