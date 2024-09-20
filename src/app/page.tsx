e68d7afac7569db1fbebed8ba977adcc58fafd1e
Pranav <pranavk24h@gmail.com>
2024-09-20 14:04:01 +0530

    change font and body bg color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Mail } from "lucide-react";
 export default function Home() {
   return (
     <>
-      <h1>Welcome to Pizza Shop</h1>
+      <h1 className="font-manrope">Welcome to Pizza Shop</h1>
       <Button>
       <Mail className="mr-2 h-4 w-4" /> Login with Email
     </Button>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-
+import {fontFamily} from 'tailwindcss/defaultTheme'
 const config: Config = {
     darkMode: ["class"],
     content: [
@@ -9,6 +9,9 @@ const config: Config = {
   ],
   theme: {
   	extend: {
+		fontFamily: {
+			manrope: ['var(--font-manrope)', ...fontFamily.sans],
+		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
